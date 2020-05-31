199e616c89255f7aea819d0fdb57b2370a8a4e0b
Claudio Zanettini <claudio.zanettini@gmail.com>
2020-05-31 01:25:16 -0400

    fist draft of deck

---
+++ pokerpy/Deck.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+
+class Deck(pd.DataFrame):
+    """" Deck of cards a Pandas dataframe
+
+    A dataframe called cards containing 3 columns, with each row representing a card:
+        cardNumber :  numeric, J, Q, K, Ace are 11, 12, 13, 14
+        suit :  1, 2, 3, 4, equivalent to clubs, diamonds, hearts, spades.
+        fullCard : as it would be show in the console if playing with symbols for the suits and J, K, Q
+                   {"Hearts":"♡", "Spades":"♠", "Diamonds":"♢", "Clubs":"♣"}
+
+    Why 3 columns? so it is easier to sort order cards, and easier to check suits, but still you can report the card
+    as a symbol. Finally, it since you don't work with 3 separate arrays, it is less prone to errors.
+
+    Parameters:
+        numCards : number of cards for each suit (default is 13). If less than for 13, cards will be removed starting from
+             2 and up.
+        suits : number unique suits (defalut is 4). Is there even a case where they are less than 4?
+        # (not implemented yet) numJokers: number of jokers. This cards are represented as 0
+
+     """
+
+    def create_deck(numCards, suits):
+        start = 16 - numCards
+        cardNumber = np.arange(start, 15, 1).repeat(4)
+        suits = np.arange(1, 5, 1).repeat(cardNumber.size / 4)
+        # than I need a recode and paste0() or whatever are the correspective in python
+
+    def __init__ (self):
+        self.cards = []
+
--- pokerpy/deck.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-class deck(pd.DataFrame):
-    "Deck is something something"
-      # def __init__(self):
-        pass
