20b5aa13b8c1a45274a075920cea5a1b2561c62e
“DottorDav” <dav79@live.it>
2020-06-21 23:56:50 +0200

    before leaving rankOfKind shit

--- pokerpy/__init__.py
+++ pokerpy/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['TO_DO.py', 'consts.py', 'Converter', 'dealer.py', 'cards_many.py', 'money.py', 'referee.py',
+__all__ = ['consts.py', 'Converter', 'dealer.py', 'cards_many.py', 'money.py', 'referee.py',
            'players.py', 'rules.py', 'card_single.py']
